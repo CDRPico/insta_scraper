bf2b9c536700ed7f32c0fd3bd58ddc07ef6897b8
cristiandramirezp@gmail.com <cristiandramirezp@gmail.com>
2023-11-21 23:32:10 -0500

    scrape via instaloader

--- instagram_scraper.py
+++ instagram_scraper.py
@@ -1,35 +1,28 @@
-from unicodedata import name
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-import time
-import urllib.request
-import requests
-import ssl
-from dotenv import load_dotenv
+from instaloader import Instaloader, Profile
+from pydub import AudioSegment
+
+import speech_recognition as sr
 import os
 
-load_dotenv()
-INSTAGRAM_USER = os.getenv("INSTAGRAM_USER")
-INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
+def download_videos(username):
+    l = Instaloader()
+    profile = Profile.from_username(l.context, username)
+    for post in profile.get_posts():
+        filename=post.date_utc.strftime('%Y-%m-%d %H-%M-%S')
+        if post.is_video:
+            l.download_post(post, target=filename)
+            print(f"Downloaded: {filename}.mp4")
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless=false')
-
-PATH=os.getenv("WEBDRIVER_PATH")
-service = Service(executable_path=PATH)
-driver = webdriver.Chrome(service=service, options=options)
-
-driver.get("https://www.instagram.com/")
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-# login
-time.sleep(4)
-username = driver.find_element("css selector", "input[name='username']")
-password = driver.find_element("css selector", "input[name='password']")
-username.clear()
-password.clear()
-username.send_keys(INSTAGRAM_USER)
-password.send_keys(INSTAGRAM_PASSWORD)
-login = driver.find_element("css selector", "button[type='submit']").click()
+def transcript_export_video(videoname_url):
+    videoname = videoname_url.split('/')
+    video = AudioSegment.from_file(videoname_url, format="mp4")
+    audio = video.set_channels(1).set_frame_rate(16000).set_sample_width(2)
+    audio.export(videoname[-1][:-3] + 'wav', format="wav")
+    r = sr.Recognizer()
+    with sr.AudioFile(videoname[-1][:-3] + 'wav') as source:
+        audio_text = r.record(source)
+    text = r.recognize_google(audio_text, language='es-CO')
+    file_name = videoname[-1][:-3] + 'txt'
+    with open(file_name, "w") as file:
+        file.write(text)
+    os.system(f"start {file_name}")
